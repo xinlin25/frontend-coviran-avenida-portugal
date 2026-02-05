fbfdd91a5e51a787b95fa2100df2ed68d7450405
xinlin4 <xinlinchenzhang@gmail.com>
2026-02-05 08:11:29 +0100

    Cambios en tarjeta y estilos

--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TarjetaComponent } from '../../shared/tarjeta/tarjeta';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-inicio',
-  imports: [CommonModule, TarjetaComponent],
+  imports: [CommonModule, TarjetaComponent, RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
