11b04dc567a925aec70851e1c3777312c5b16eb5
xinlin4 <xinlinchenzhang@gmail.com>
2026-05-17 12:36:31 +0200

    Confirmacion contraseña + Ver contraseña

--- src/app/pages/registro/registro.ts
+++ src/app/pages/registro/registro.ts
@@ -14,29 +14,63 @@ export class Registro {
   registerForm: FormGroup;
   mensaje: string = '';
   esError: boolean = false;
+  mostrarPassword = false;
+  mostrarConfirmPassword = false;
 
   constructor(
     private fb: FormBuilder,
     private authService: Auth,
     private router: Router,
   ) {
-    this.registerForm = this.fb.group({
-      nombreCompleto: [
-        '',
-        [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
-      ],
-      tlf: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(15)]],
-      correo: [
-        '',
-        [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(100)],
-      ],
-      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(60)]],
-      direccion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-    });
+    this.registerForm = this.fb.group(
+      {
+        nombreCompleto: [
+          '',
+          [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
+        ],
+        tlf: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(15)]],
+        correo: [
+          '',
+          [
+            Validators.required,
+            Validators.email,
+            Validators.minLength(5),
+            Validators.maxLength(100),
+          ],
+        ],
+        password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(60)]],
+
+        confirmPassword: ['', [Validators.required]],
+
+        direccion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      },
+      {
+        validators: this.passwordsIguales,
+      },
+    );
   }
 
   get f() {
     return this.registerForm.controls;
+  }
+
+  passwordsIguales(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) form.get('confirmPassword')?.setErrors({ noCoinciden: true });
+    else {
+      const errors = form.get('confirmPassword')?.errors;
+
+      if (errors) {
+        delete errors['noCoinciden'];
+
+        if (Object.keys(errors).length === 0) form.get('confirmPassword')?.setErrors(null);
+        else form.get('confirmPassword')?.setErrors(errors);
+      }
+    }
+
+    return null;
   }
 
   onSubmit() {
